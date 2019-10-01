b85d0d38ab63cf7f07e1cd2a73dd50c600143191
Leonardo Augusto <leonardoaugusto@Leonardos-MacBook-Pro.local>
2019-10-01 15:16:28 -0300

    test app12

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"app12/model"
+	"fmt"
+)
+
+func main() {
+
+	jojo := model.Ave{}
+	jojo.Nome = "Jojo da silva"
+
+	queroAcordarComUmCarcarejo(jojo)
+	queroOuvirUmaPAtaNoLago(jojo)
+}
+
+func queroAcordarComUmCarcarejo(g model.Galinha) {
+	fmt.Println(g.Cacareja())
+}
+
+func queroOuvirUmaPAtaNoLago(p model.Pata) {
+	fmt.Println(p.Quack())
+}
---
+++ model/ave.go
@@ -0,0 +1,26 @@
+package model
+
+//Galinha representa uma ave do tipo galinha
+type Galinha interface {
+	Cacareja() string
+}
+
+//Pata representa uma ave
+type Pata interface {
+	Quack() string
+}
+
+//Ave representa um animal
+type Ave struct {
+	Nome string
+}
+
+//Cacareja representa o som de uma galinha
+func (a Ave) Cacareja() string {
+	return "Cócóricó..."
+}
+
+// Quack representa o soim de um pato
+func (a Ave) Quack() string {
+	return "Quack, quack..."
+}
